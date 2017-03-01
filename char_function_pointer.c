f52320c69f09de7747837b6ae1be213915c772cc
Sid <siddharthasadhukhan2009@gmail.com>
2017-03-01 18:09:28 +0530

    Add files via upload

---
+++ char_function_pointer.c
@@ -0,0 +1,21 @@
+// Program to pass charecter pointer to the Function
+
+#include<stdio.h>
+#include<stdlib.h>
+void print(char* ch)
+{
+	int i;
+	while(*(ch+i)!='\0') // Iterate until the pointer hit to the \0
+	{
+	printf("%c",*(ch+i));
+	i++;
+	}
+	printf("\n");
+	}
+
+main()
+{
+	
+	char name[]="This is a String";
+	print(name);
+}
