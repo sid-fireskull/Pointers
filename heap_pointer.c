38b8e0a9a30b6dd8b10de74b0c2707278e7cd7ad
Sid <siddharthasadhukhan2009@gmail.com>
2017-03-02 00:46:31 +0530

    Add files via upload

---
+++ heap_pointer.c
@@ -0,0 +1,27 @@
+// Using of Heap Pointers to store Data
+#include<stdio.h>
+#include<stdlib.h>
+
+int *mysum(int *x, int *y)
+{
+	// malloc function is use to block a memory block in heap which is use to store data permanently to the entire program lifecycle
+	
+	int* val=(int *)malloc(sizeof(int)); 
+	*val=(*x)+(*y);
+	return val;
+}
+
+main()
+{
+	int a,b;
+	int *c;
+	printf("Enter the first number: ");
+	scanf("%d",&a);
+	printf("\nEnter the Second number: ");
+	scanf("%d",&b);
+	c=mysum(&a,&b);
+	printf("\nAnswer: %d ");
+	getch();
+}
+
+
